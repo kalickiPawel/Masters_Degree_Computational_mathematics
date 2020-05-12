2a44bb81105611ca0851d93ba0ef5bd3695c80e3
Paweł <kalicki.paul@icloud.com>
2020-05-12 10:52:48 +0200

    Added lab02 with exercise01

---
+++ matlab/02_errors_02/exercise01.m
@@ -0,0 +1,66 @@
+clc
+close all
+clear all
+
+format long e
+
+x=10^6;
+x=single(x);
+
+tab=[1:1:x];
+n=length(tab);
+
+A1=0;
+A2=0;
+A3=0;
+A4=0;
+
+% --Adding the sum of subsequent items in a loop--%
+tic
+for i=1:n
+    A1=A1+tab(i);
+end
+t1 = toc;
+
+% --sum() function--%
+tic
+A2=sum(tab);
+t2 = toc;
+
+% --Kahan summation algorithm-- %
+tic
+S=tab(1);
+C=0;
+
+for i=2:n
+    Y=tab(i)-C;
+    T=S+Y;
+    C=(T-S)-Y;
+    S=T;
+end
+A3=S;
+t3 = toc;
+
+% --Gill-Moller algorithm-- %
+tic
+S=0;
+U=0;
+P=0;
+for i=1:n
+    S=U+tab(i);
+    P=U-S+tab(i)+P;
+    U=S;
+end
+S=S+P;
+A4=S;
+t4 = toc;
+
+% --sum of elements of the string-- %
+S_n = (tab(1)+tab(x))/2*n;
+S_n = single(S_n);
+
+ans = [abs(A1-S_n) abs(A2-S_n) abs(A3-S_n) abs(A4-S_n)];
+axes_x = categorical({'A1: loop','A2: sum()','A3:Kahan','A4: Gill-Moller'});
+
+bar(axes_x, ans)
+title('Graph of calculation of string sums (single prec)')
